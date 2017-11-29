d3161f86b5529638b0deee515acd4f73ab4f80f3
Unknown <pottsbrendan@rocketmail.com>
2017-11-29 23:29:32 +0000

    added main
    Video wont run on laptop but will on desktop

--- video_in.py
+++ video_in.py
@@ -4,12 +4,14 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+if __name__=='__main__':
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
 
-cap.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
